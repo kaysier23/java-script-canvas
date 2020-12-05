fe6ac5d2ae541621a94015e573740f47a927e5c3
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-05 22:32:36 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title> canvas </title>
+</head>
+<script type="application/processing">
+size(200, 200);
+
+fill(0, 255, 0);
+
+ellipse(100, 100, 150, 150);
+
+fill(255);
+
+ellipse(75, 85, 30, 20);
+ellipse(125, 85, 30, 20);
+
+fill(0);
+
+ellipse(75, 85, 10, 10);
+ellipse(125, 85, 10, 10);
+
+fill(255, 0, 0);
+
+arc(100, 125, 80, 50, 0, 3.14);
+line(60, 125, 140, 125);
+</script>
+<body>
+<canvas> </canvas>
+</body>
+</html>
