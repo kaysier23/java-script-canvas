85bba039694013792badd038f575de84306dbc70
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-05 22:36:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,8 @@
 <html>
-<script type="application/processing">
-size(200, 200);
-
-fill(0, 255, 0);
-
-ellipse(100, 100, 150, 150);
-
-fill(255);
-
-ellipse(75, 85, 30, 20);
-ellipse(125, 85, 30, 20);
-
-fill(0);
-
-ellipse(75, 85, 10, 10);
-ellipse(125, 85, 10, 10);
-
-fill(255, 0, 0);
-
-arc(100, 125, 80, 50, 0, 3.14);
-line(60, 125, 140, 125);
-</script>
-<canvas> </canvas>
+    <head>
+    </head>
+    <body>
+        <canvas id="cvs">
+        </canvas>
+    </body>
 </html>
